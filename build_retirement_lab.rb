5a7e38d224c4f34253a99cf37c0ee58b2ad7099a
JayTeeSF <jaytee@jayteesf.com>
2026-04-07 22:22:43 -0700

    wip: improve income modeling

--- build_retirement_lab.rb
+++ build_retirement_lab.rb
@@ -2,7 +2,6 @@
 require 'json'
 require 'erb'
 require 'pathname'
-require 'time'
 
 root = Pathname.new(File.expand_path(File.dirname(__FILE__)))
 template_path = root.join('retirement_scenario_lab_template.html.erb')
@@ -14,14 +13,6 @@ output_path = Pathname.new(ARGV[3] || root.join('retirement_scenario_lab.html'))
 embedded_tax_data_json = JSON.pretty_generate(JSON.parse(tax_path.read))
 embedded_defaults_json = JSON.pretty_generate(JSON.parse(defaults_path.read))
 embedded_sample_scenario_json = JSON.pretty_generate(JSON.parse(sample_path.read))
-embedded_build_metadata_json = JSON.pretty_generate({
-  built_at_utc: Time.now.utc.iso8601,
-  template_file: template_path.basename.to_s,
-  tax_file: tax_path.basename.to_s,
-  defaults_file: defaults_path.basename.to_s,
-  sample_file: sample_path.basename.to_s,
-  output_file: output_path.basename.to_s
-})
 
 renderer = ERB.new(template_path.read, trim_mode: '-')
 html = renderer.result(binding)
